84bf37266c8f896b18ce8110b5bbfd17a12b5614
Az <hereliesaz@gmail.com>
2022-11-24 07:20:50 -0600

    ohm
     ohm

---
+++ ohm/app.js
@@ -0,0 +1,28 @@
+///Edit Under This only
+
+  
+    var firebaseConfig = {
+    apiKey: "API KEY",
+    authDomain: "AUTH DOMAIN",
+    databaseURL: "DATABSE URL",
+    projectId: "PROJECT ID",
+    storageBucket: "BUCKET URL",
+    messagingSenderId: "SENDER ID",
+    appId: "APP ID"
+  }; 
+  
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCF7Rqb11cHAxi3RLGgtZOsJuR9CsdYZWA",
+  authDomain: "ohmrat-ce9e3.firebaseapp.com",
+  databaseURL: "https://ohmrat-ce9e3-default-rtdb.firebaseio.com",
+  projectId: "ohmrat-ce9e3",
+  storageBucket: "ohmrat-ce9e3.appspot.com",
+  messagingSenderId: "898595961438",
+  appId: "1:898595961438:web:44e8b546f8ac4610ced2cf",
+  measurementId: "G-325S58HVQ0"
+};
+  
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
