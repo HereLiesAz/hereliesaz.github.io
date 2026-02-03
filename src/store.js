4a507add4f7e1287115bcce388260e007d4ffa88
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-02-03 04:25:00 -0600

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,1 +1,13 @@
+import { create } from 'zustand';
 
+export const useStore = create((set) => ({
+  // The raw Z-position of the camera
+  scrollZ: 0,
+  
+  // The index of the painting currently in the "Focus Window"
+  activePaintingIndex: null,
+  
+  // Actions
+  setScrollZ: (z) => set({ scrollZ: z }),
+  setActivePainting: (index) => set({ activePaintingIndex: index }),
+}));
